a37dced9268c755520170c632f751e96e73fbda2
J <flatironschool@2joyce.com>
2018-01-14 06:47:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,3 +7,14 @@ function lastImageSelector() {
   return $('img:last');
 }
 
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstLineItem() {
+  return $('ul #pic-list:first-child');
+}
